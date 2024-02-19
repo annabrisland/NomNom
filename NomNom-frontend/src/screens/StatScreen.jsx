a16c358755b1bca8008ba542ebb93e010f9837ec
Harin <master.harin@gmail.com>
2024-02-18 22:29:51 -0800

    DynamoDB connection

--- NomNom-frontend/src/screens/StatScreen.jsx
+++ NomNom-frontend/src/screens/StatScreen.jsx
@@ -1,68 +1,23 @@
 import "../App.css";
 import { useEffect, useState } from "react";
-import { DynamoDBClient, ListBackupsCommand } from "@aws-sdk/client-dynamodb";
-import ApexCharts from "apexcharts";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import ReactApexChart from "react-apexcharts";
 
 function StatScreen() {
-  useEffect(() => {
-    const client = new DynamoDBClient({ region: "us-east-1" });
-    const command = new ListBackupsCommand({ TableName: "BiteSnap" });
-    client.send(command).then(
-      (data) => console.log(data.BackupSummaries),
-      (error) => console.error(error)
-    );
-  }, []);
-
-  const users = ["harin", "anna"];
   const [selectedUser, setSelectedUser] = useState("harin");
-  const [costSeries, setCostSeries] = useState([]);
   const [cuisineSeries, setCuisineSeries] = useState([]);
-  const costChartOptions = {
-    chart: {
-      height: 350,
-      type: "line",
-      zoom: {
-        enabled: false,
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "straight",
-    },
-    title: {
-      text: "Product Trends by Month",
-      align: "left",
-    },
-    grid: {
-      row: {
-        colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-        opacity: 0.5,
-      },
-    },
-    xaxis: {
-      categories: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-      ],
-    },
-  };
+  const [cuisineList, setCuisineList] = useState([]);
+  const [last3Eaten, setLast3Eaten] = useState([]);
+  const [mostConsumedProtein, setMostConsumedProtein] = useState("");
+  const [leastConsumedProtein, setLeastConsumedProtein] = useState("");
 
   const cuisineChartOptions = {
     chart: {
       width: 380,
       type: "pie",
     },
-    labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+    labels: cuisineList,
     responsive: [
       {
         breakpoint: 480,
@@ -78,50 +33,72 @@ function StatScreen() {
     ],
   };
 
-  const dummyData = [
-    {
-      entry_id: 1,
-      user_id: "harin",
-      cost: 20.29,
-      cuisine: "Chinese",
-      protein_consumed: ["pork"],
-      restaurant_name: "Happy Noodle House",
-      date: "2021-10-01",
-    },
-    {
-      entry_id: 2,
-      user_id: "harin",
-      cost: 15.29,
-      cuisine: "Italian",
-      protein_consumed: ["beef"],
-      restaurant_name: "Pasta Palace",
-      date: "2021-10-02",
-    },
-    {
-      entry_id: 3,
-      user_id: "harin",
-      cost: 30.29,
-      cuisine: "Mexican",
-      protein_consumed: ["chicken"],
-      restaurant_name: "Taco Time",
-      date: "2021-10-03",
-    },
-    {
-      entry_id: 4,
-      user_id: "harin",
-      cost: 25.29,
-      cuisine: "Japanese",
-      protein_consumed: ["fish"],
-      restaurant_name: "Sushi Bar",
-      date: "2021-10-04",
-    },
-  ];
+  async function getStats(user_id) {
+    const accessKey = await import.meta.env.VITE_AWS_ACCESS_KEY_ID;
+    const secretKey = await import.meta.env.VITE_AWS_SECRET_ACCESS_KEY;
+    const client = new DynamoDBClient({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secretKey,
+      },
+    });
+    const docClient = DynamoDBDocumentClient.from(client);
+    const input = {
+      TableName: "NomNom_Entry",
+      KeyConditionExpression: "user_id = :id",
+      ExpressionAttributeValues: {
+        ":id": user_id,
+      },
+    };
+    const command = new QueryCommand(input);
+    const data = await docClient.send(command);
+    console.log(data.Items);
+
+    const cuisineList = data.Items.map((item) => item.cuisine);
+    const uniqueCuisines = [...new Set(cuisineList)];
+    setCuisineList(uniqueCuisines);
+
+    const cuisineCount = {};
+    for (let i = 0; i < cuisineList.length; i++) {
+      cuisineCount[cuisineList[i]] = (cuisineCount[cuisineList[i]] || 0) + 1;
+    }
+    const cuisineSeries = Object.values(cuisineCount);
+    setCuisineSeries(cuisineSeries);
+
+    const lastEatenMap = {};
+    for (let i = 0; i < data.Items.length; i++) {
+      const item = data.Items[i];
+      if (lastEatenMap[item.cuisine]) {
+        if (item.date < lastEatenMap[item.cuisine].date) {
+          lastEatenMap[item.cuisine] = item;
+        }
+      } else {
+        lastEatenMap[item.cuisine] = item;
+      }
+    }
+
+    const last3Eaten = Object.entries(lastEatenMap)
+      .sort((a, b) => {
+        return new Date(b[1].date) - new Date(a[1].date);
+      })
+      .slice(0, 3);
+    setLast3Eaten(last3Eaten);
+
+    const proteinList = data.Items.map((item) => [...item.protein_consumed]);
+    const proteinCount = {};
+    for (let i = 0; i < proteinList.length; i++) {
+      proteinCount[proteinList[i][0]] =
+        (proteinCount[proteinList[i][0]] || 0) + 1;
+    }
+    const proteinCountArray = Object.entries(proteinCount);
+    proteinCountArray.sort((a, b) => b[1] - a[1]);
+    setMostConsumedProtein(proteinCountArray[0][0]);
+    setLeastConsumedProtein(proteinCountArray[proteinCountArray.length - 1][0]);
+  }
 
   useEffect(() => {
-    const costSeries = dummyData
-      .filter((entry) => entry.user_id === selectedUser)
-      .map((entry) => entry.cost);
-    setCostSeries(costSeries);
+    getStats(selectedUser);
   }, [selectedUser]);
 
   return (
@@ -133,18 +110,25 @@ function StatScreen() {
         <div>Placeholder</div>
       </div>
       {/* Who Dropdown */}
-      <div className="whoDropdown">Who Dropdown</div>
-      {/* Cost Graph */}
       <div className="whoDropdown">
-        <ReactApexChart
-          options={costChartOptions}
-          series={costSeries}
-          type="line"
-          height={350}
-        />
+        <div
+          onClick={() => {
+            setSelectedUser("harin");
+          }}
+        >
+          Harin
+        </div>
+        <div
+          onClick={() => {
+            setSelectedUser("anna");
+          }}
+        >
+          Anna
+        </div>
       </div>
       {/* Cuisine Pie Chart */}
-      <div className="whoDropdown">
+      <div className="pieChartArea">
+        {console.log(cuisineChartOptions)}
         <ReactApexChart
           options={cuisineChartOptions}
           series={cuisineSeries}
@@ -153,10 +137,25 @@ function StatScreen() {
         />
       </div>
       {/* LRU Cuisine */}
-      <div className="whoDropdown">You haven't had these in a while!</div>
+      <div className="whoDropdown">
+        <div>You have not had these in a while!</div>
+        <div>
+          {last3Eaten.map((item) => {
+            return (
+              <div key={item[1].date}>
+                {item[1].cuisine} - {item[1].restaurant_name}
+              </div>
+            );
+          })}
+        </div>
+      </div>
       {/* Protein */}
       <div className="whoDropdown">
-        Most Consumed Protein vs Least Consumed Protein
+        <div>Most Consumed Protein vs Least Consumed Protein</div>
+        <div>
+          <div>{mostConsumedProtein}</div>
+          <div>{leastConsumedProtein}</div>
+        </div>
       </div>
     </div>
   );
