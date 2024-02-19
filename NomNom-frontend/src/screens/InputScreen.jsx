7af2dea711b33bdedba1eb3f0b384e519f267404
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-02-18 22:29:56 -0800

    Adds styles to Input page

--- NomNom-frontend/src/screens/InputScreen.jsx
+++ NomNom-frontend/src/screens/InputScreen.jsx
@@ -8,43 +8,63 @@ function InputScreen() {
         Stats
       </header>
       <form>
-        <label for="where">Where did you eat?</label>
-        <input type="text" id="where"></input>
-        <label for="cost">How much did you spend?</label>
-        $
-        <input type="number" id="cost"></input>
-        <label for="cuisine">What cuisine did you eat?</label>
-        <input type="text" id="cuisine"></input>
+        <div className="section">
+        <label for="user">Who are you?</label>
+        <div className="user-container">
+            <div className="user">👦🏻 &nbsp;Harin</div>
+            <div className="user">👧🏻 &nbsp;Anna</div>
+        </div>
+        </div>
+        <div className="input-wrapper">
+          <div className="input">
+            <input type="text" id="where" required placeholder=" "></input>
+            <label for="where">Where did you eat?</label>
+          </div>
+        </div>
+        <div className="input-wrapper">
+          <div className="input">
+            <input type="number" id="cost" required placeholder=" "></input>
+            <label for="cost">How much did you spend?</label>
+          </div>
+        </div>
+        <div className="input-wrapper">
+          <div className="input">
+            <input type="text" id="cuisine" required placeholder=" "></input>
+            <label for="cuisine">What cuisine did you eat?</label>
+          </div>
+        </div>
+        <div className="section">
         <label for="protein-list">What protein did you eat?</label>
         <ul className="protein-list">
           <li className="protein-list-item" id="chicken">
-          🐔 Chicken
+            🐔 Chicken
           </li>
           <li className="protein-list-item" id="beef">
-          🐮 Beef
+            🐮 Beef
           </li>
           <li className="protein-list-item" id="lamb">
-          🐑 Lamb
+            🐑 Lamb
           </li>
           <li className="protein-list-item" id="turkey">
-          🦃 Turkey
+            🦃 Turkey
           </li>
           <li className="protein-list-item" id="pork">
-          🐷 Pork
+            🐷 Pork
           </li>
           <li className="protein-list-item" id="duck">
-          🦆 Duck
+            🦆 Duck
           </li>
           <li className="protein-list-item" id="fish">
-          🐟 Fish
+            🐟 Fish
           </li>
           <li className="protein-list-item" id="seafood">
-          🍤 Seafood
+            🍤 Seafood
           </li>
           <li className="protein-list-item" id="plantbased">
-          🌱 Plant Based
+            🌱 Plant Based
           </li>
         </ul>
+        </div>
         <button className="submit-btn">Submit Experience</button>
       </form>
     </div>
